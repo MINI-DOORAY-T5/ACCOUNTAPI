930a84018b5f08b9120cd3ad591bc87d9fa4249c
{Hongkyoungseuk} <tug234@daum.net>
2024-01-26 12:59:26 +0900

    DTO 수정

---
+++ src/main/java/com/nhnacademy/minidooray/accountapi/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray.accountapi.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserDTO {
+    private String id;
+
+    private String email;
+
+    private String state;
+
+}
